6733482eb0b273ba2ec1225c10a1f236f736f2e9
Gurvinder135 <83618057+Gurvinder135@users.noreply.github.com>
2022-11-02 22:32:12 -0400

    added vercel.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.URL, "https://friendbookclient.vercel.app/"],
+    origin: "*",
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
     credentials: true,
   })
