fcf152babce02fc2e26349a49963ce03f3f7914c
Gurvinder135 <83618057+Gurvinder135@users.noreply.github.com>
2022-11-03 23:26:45 -0400

    added vercel.json

--- controller/controller.js
+++ controller/controller.js
@@ -19,6 +19,8 @@ module.exports.postLogin = async (req, res, next) => {
   res
 
     .cookie("Token", token, {
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     })
     .status(200)
